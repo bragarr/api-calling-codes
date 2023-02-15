7533cb885be1caa5f43f0bdd5e91f0d9f0bec753
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2023-02-15 18:09:26 -0400

    fix: file extension name controller/routes

--- routes/callingcodes.js
+++ routes/callingcodes.js
@@ -3,5 +3,4 @@ import { getAllCallingCodes } from "../controller/callingCode.js";
 
 export const routerCallingCodes = express.Router();
 
-// Rotas para criação de categorias
 routerCallingCodes.get("/", getAllCallingCodes);
